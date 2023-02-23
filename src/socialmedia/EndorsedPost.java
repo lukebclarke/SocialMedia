04588bb1fb057b3b896f661a870728c77efc594e
Barnesey41 <74460619+Barnesey41@users.noreply.github.com>
2023-02-23 14:57:05 +0000

    Implemented endorsed post class

--- src/socialmedia/EndorsedPost.java
+++ src/socialmedia/EndorsedPost.java
@@ -1,5 +1,48 @@
 package socialmedia;
 
 public class EndorsedPost extends Post {
+    public EndorsedPost(Account authorObject, Post postObject) {
+        super(authorObject, postObject.getMessage());
+        postObject.addEndorsement(this);
+    }
+
+    /**<p>DO NOT USE</p>
+     * Overrides the existing superclass method, outputs a warning message to the console:
+     * "The operation: 'addComment' cannot be performed on an endorsed post.\n
+     *  operation FAILED!"
+    */
+    public void addComment(Comment commentObject) {
+        System.out.println("The operation: 'addComment' cannot be performed on an endorsed post.\noperation FAILED!");
+    }
+
+    /**<p>DO NOT USE</p>
+     * Overrides the existing superclass method, outputs a warning message to the console:
+     * "The operation: 'removeComment' cannot be performed on an endorsed post.\n
+     *  operation FAILED!"
+    */
+    public void removeComment(Comment commentObject) {
+        System.out.println("The operation: 'removeComment' cannot be performed on an endorsed post.\noperation FAILED!");
+    }
+
+    /**<p>DO NOT USE</p>
+     * Overrides the existing superclass method, outputs a warning message to the console:
+     * "The operation: 'addEndorsement' cannot be performed on an endorsed post.\n
+     *  operation FAILED!"
+    */
+    public void addEndorsement(EndorsedPost endorsedPostObject) {
+        System.out.println("The operation: 'addEndorsement' cannot be performed on an endorsed post.\noperation FAILED!");
+    }
+
+    /**<p>DO NOT USE</p>
+     * Overrides the existing superclass method, outputs a warning message to the console:
+     * "The operation: 'removeComment' cannot be performed on an endorsed post.\n
+     *  operation FAILED!"
+    */
+    public void removeEndorsement(EndorsedPost endorsedPostObject) {
+        System.out.println(
+                "The operation: 'removeEndorsement' cannot be performed on an endorsed post.\noperation FAILED!");
+    }
+
     
+    //TODO better implementation of removing the ability to endorse and comment on posts
 }
