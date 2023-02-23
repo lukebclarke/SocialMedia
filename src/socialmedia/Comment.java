604eec91f980c10a91eae5af0fa42ad973cdc6e8
Barnesey41 <74460619+Barnesey41@users.noreply.github.com>
2023-02-23 14:57:13 +0000

    implemented comment class

--- src/socialmedia/Comment.java
+++ src/socialmedia/Comment.java
@@ -1,5 +1,8 @@
 package socialmedia;
 
 public class Comment extends Post {
-
+    public Comment(Account authorObject, Post postCommentingToObject, String messageToComment) {
+        super(authorObject, messageToComment);
+        postCommentingToObject.addComment(this);
+    }
 }
