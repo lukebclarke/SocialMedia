e313b6eda50e55cfcb5921bcd3dbabc352099096
lukebclarke <luke616logins@gmail.com>
2023-02-25 07:36:10 +0000

    creating account class

--- src/socialmedia/Account.java
+++ src/socialmedia/Account.java
@@ -1,5 +1,37 @@
 package socialmedia;
 
+//import java.util.ArrayList;
+
 public class Account {
+    private int accountID;
+    private String handle;
+    private String description;
+    //private ArrayList<Post> Posts; //used to track which posts the author has for easy deletion (not sure if needed - TODO: double check)
+
+    public int getAccountID() {
+        return accountID;
+    }
+
+    public String getHandle() {
+        return handle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setAccountID(int newID) {
+        accountID = newID;
+    }
+
+    public void setHandle(String newHandle) {
+        handle = newHandle;
+    }
+
+    public void setDescription(String newDescription) {
+        description = newDescription;
+    }
+
+    //TODO: write function to delete account/all posts
 
 }
