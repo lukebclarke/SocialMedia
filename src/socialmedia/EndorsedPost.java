8261c398f3afffffbe75723b39633017edda2c91
Barnesey41 <74460619+Barnesey41@users.noreply.github.com>
2023-02-28 21:44:49 +0000

    code clean up

--- src/socialmedia/EndorsedPost.java
+++ src/socialmedia/EndorsedPost.java
@@ -3,7 +3,7 @@ package socialmedia;
 public class EndorsedPost extends Post {
     public EndorsedPost(Account authorObject, Post postObject) throws InvalidPostException {
         super(authorObject, postObject.getMessage());
-        postObject.addEndorsement(this);
+        postObject.addEndorsedPost(this);
     }
 
     @Override
@@ -33,7 +33,7 @@ public class EndorsedPost extends Post {
      * "The operation: 'addEndorsement' cannot be performed on an endorsed post.\n
      *  operation FAILED!"
     */
-    public void addEndorsement(EndorsedPost endorsedPostObject) {
+    public void addEndorsedPost(EndorsedPost endorsedPostObject) {
         System.out
                 .println("The operation: 'addEndorsement' cannot be performed on an endorsed post.\noperation FAILED!");
     }
@@ -44,9 +44,9 @@ public class EndorsedPost extends Post {
      * "The operation: 'removeComment' cannot be performed on an endorsed post.\n
      *  operation FAILED!"
     */
-    public void removeEndorsement(EndorsedPost endorsedPostObject) {
+    public void removeEndorsedPost(EndorsedPost endorsedPostObject) {
         System.out.println(
-                "The operation: 'removeEndorsement' cannot be performed on an endorsed post.\noperation FAILED!");
+                "The operation: 'removeEndorsedPost' cannot be performed on an endorsed post.\noperation FAILED!");
     }
 
     
